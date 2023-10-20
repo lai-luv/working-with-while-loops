17f82498027a09fde9f12cd64f2cb2c695eef15c
lai_luv <l.lavaughn319@gmail.com>
2023-10-20 12:38:51 -0400

    changes

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
 /****************** Exercise 1 ********************/
 
 // List five examples of websites using loops. 
-  
 
-
-
+// Amazon  
+// BJ'S
+// Costco's
+// Facebook
+// Google
 
 
 
 /****************** Exercise 2 ********************/
 
 // Use a while loop to log to the console the numbers from 1 to 10
-
-
-
-
+let integer = 1
+while(integer < 11){
+console.log(`integer: ${integer}`)
+integer++
+}
 
 
 
@@ -22,7 +25,16 @@
 
 // Use a while loop to calculate the sum of even numbers from 2 to 20
 
+let sum = 2;
+let num = 0;
 
+while(sum<= 22){
+if (sum % 2===0){
+    num +=sum;
+}
+sum++
+}
+console.log(`Sum of even numbers from 2 to 22 is :${num}`)
 
 
 
@@ -34,8 +46,10 @@
 let countdown = 10;
 
 
-
-
+while(countdown>=1){
+    countdown--;
+}
+console.log(countdown,"blast off")
 
 
 
@@ -44,19 +58,28 @@ let countdown = 10;
 /****************** Exercise 5 ********************/
 
 // Review This Code
-const secretNumber = Math.floor(Math.random() * 100) + 1; 
-let guess;
-let attempts = 0;
 
-while (guess !== secretNumber) {
-    guess = parseInt(prompt("Guess a number between 1 and 100:"));
+// const secretNumber = Math.floor(Math.random() * 100) + 1; 
+// let guess;
+// let attempts = 0;
 
-    if (isNaN(guess)) {
-        console.log("Please enter a valid number.");
-    } else {
-        attempts++;
-    }
-}
+// while (guess !== secretNumber) {
+//     guess = parseInt(prompt("Guess a number between 1 and 100:"));
+
+//     if (isNaN(guess)) {
+//         alert("Please enter a valid number.");
+//     } else {
+//         attempts++;
+//         if(guess < secretNumber){
+//             alert('too low')
+//         }else if(guess > secretNumber){
+//             alert('too high!')
+//         }else{
+//             alert(`Congratulations! you guessed the number ${secretNumber} in ${attempts} attempts.`)
+//         }
+//     }
+    
+// }
 
 
 // Refactor the code so that:
@@ -64,7 +87,7 @@ while (guess !== secretNumber) {
 // 2. The user will see the alert "Too high!" if the number is too high. 
 // 3. The user will see the alert "`Congratulations! You guessed the number [NUMBER] in [ATTEMPTS] attempts.`)" if the number is correct. 
 
-
+ 
 
 
 /****************** Exercise 6 ********************/
@@ -80,8 +103,8 @@ const itemPrice = 3;
 console.log("Welcome to the Vending Machine!");
 console.log(`You have $${userBalance} in your balance.`);
 
-while (//YOUR CODE HERE//) {
-    console.log("Available options:");
+while(userBalance >= itemPrice) {
+    console.log("Available options:")
     console.log("1. Snack A ($3)");
     console.log("2. Snack B ($2)");
     console.log("3. Exit");
